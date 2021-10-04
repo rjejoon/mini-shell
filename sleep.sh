66ea8f9d334f959d7e8e5bc10881fd5de3b68fb1
Jejoon Ryu <rjejoon@gmail.com>
2021-10-03 20:49:10 -0600

    Remove test files

--- sleep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read SLEEP_TIME
-echo "Hi, I'm sleeping for ${SLEEP_TIME} seconds ..."
-sleep ${SLEEP_TIME}
-echo "Done sleep"
--- sleeper.py
+++
@@ -1,7 +0,0 @@
-import time
-
-
-sec = int(input("Enter time in seconds to sleep: "))
-time.sleep(sec)
-
-print(f"Slept for {sec}seconds!")
